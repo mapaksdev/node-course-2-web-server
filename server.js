c0d124798d9d61be95f7a3e3052ad1bf98c766f4
Mapaks <nlumapac@gmail.com>
2017-07-21 15:33:34 +0800

    Add projects page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    someMessage: 'Portfolio page here!'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request.'
